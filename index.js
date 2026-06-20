76e57071b017e3db8039232ce603e7f218387876
Mark <125941419+Rondepart@users.noreply.github.com>
2026-06-20 20:32:16 +0800

    Add event listener to canvas size button showing a prompt to change grid count of canvas

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ gridContainer.style.width = '960px';
 gridContainer.style.height = '960px';
 gridContainer.style.border = '5px solid black';
 
-for (let row = 0; row < 16; row++) {
-  const rowDiv = document.createElement('div');
-  rowDiv.classList.add('row');
-  rowDiv.style.display = 'flex';
-  rowDiv.style.flex = '1';
-  rowDiv.style.width = '100%';
+function generateGridCells(cellCount) {
+  gridContainer.replaceChildren();
 
-  for (let col = 0; col < 16; col++) {
+  for (let row = 0; row < cellCount; row++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    rowDiv.style.display = 'flex';
+    rowDiv.style.flex = '1';
+    rowDiv.style.width = '100%';
+
+  for (let col = 0; col < cellCount; col++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.style.flex = '1';
@@ -22,11 +25,29 @@ for (let row = 0; row < 16; row++) {
     rowDiv.appendChild(cell);
   }
 
-  gridContainer.appendChild(rowDiv);
-}
+    gridContainer.appendChild(rowDiv);
+  }
+} 
+
+generateGridCells(16);
 
 gridContainer.addEventListener('mouseover', function(event) {
   if (event.target !== gridContainer) {
       event.target.style.background = 'black';
+  }
+})
+
+document.querySelector('.change-canvas-size').addEventListener('click', function(event) {
+  const input = prompt('Enter grid count. (Max 100)');
+  const cellCount = Number(input);
+
+  if (input === null) return;
+
+  if (isNaN(cellCount) || input.trim() === '') {
+    alert('Please enter a valid number!');
+  } else if (cellCount > 100){
+    alert('Max grid size is 100!');
+  } else {
+    generateGridCells(cellCount);
   }
 })
